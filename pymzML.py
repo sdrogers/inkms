a06ca475601bab7e148b81816164ee76e952408e
constambeys <timo0190@gmail.com>
2016-06-01 16:52:40 +0100

    update

---
+++ pymzML.py
@@ -0,0 +1,61 @@
+import pymzml
+import numpy as np
+import matplotlib.pyplot as plt
+
+lines = 8
+# Read File
+run = pymzml.run.Reader('data\\abcdefgh_1.mzML', MS1_Precision=5e-6)
+
+# -----------------------------------------------------------------------
+# Maximum peaks in 1d array
+scans = 0
+x = []
+t = []
+for spectrum in run:
+
+    max_mz = spectrum.peaks[0][0]
+    max_i = spectrum.peaks[0][1]
+    for mz, i in spectrum.peaks:
+        if max_i < i:
+            max_i = i
+            max_mz = mz
+            # print(type(mz), type(i))  # float, float
+            # print(mz, i)
+    x.append(max_mz)
+    t.append(max_i)
+    scans = scans + 1
+
+# -----------------------------------------------------------------------
+scansPerLine = scans / lines
+if not scansPerLine.is_integer():
+    raise Exception('Pixels per line not integer value')
+scansPerLine = int(scansPerLine)
+
+# data[line] = [[[m/z, value],...,[m/z, value]],[[m/z, value],]]
+data = []
+direction = True  # forward /  backward
+for line in range(0, lines):
+    data.append([])
+    if direction:
+        for scan in range(0, scansPerLine):
+            data[line].append([x[line * scansPerLine + scan], t[line * scan + scan]])
+            # print(line * scansPerLine + scan, "\n")
+    else:
+        for scan in reversed(range(0, scansPerLine)):
+            data[line].append([x[line * scansPerLine + scan], t[line * scan + scan]])
+            # print(line * scansPerLine + scan, "\n")
+    direction = not direction
+
+# -----------------------------------------------------------------------
+
+np_data = np.array(data)
+Z = np_data[:, :, 1]
+
+# http://stackoverflow.com/questions/22121239/matplotlib-imshow-default-colour-normalisation
+plt.figure()
+plt.imshow(Z, cmap=plt.get_cmap('hot'), origin='upper', interpolation='nearest',
+           vmin=0, vmax=Z.max(), extent=[0, 790, 0, 7])
+plt.colorbar()
+plt.show()
+
+print("Finished")
